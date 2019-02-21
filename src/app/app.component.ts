8cbcb38055d3faa60b0c0698bab5951e204a919b
Ankur Atri <ankur@zenways.io>
2019-02-21 17:24:05 +0530

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { HttpClient } from '@angular/common/http';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PaymentModalComponent } from './payment-modal/payment-modal.component';
 
+import { get } from 'scriptjs';
+declare var Razorpay: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,6 +16,7 @@ export class AppComponent implements OnInit {
   title = 'razorpay-angular-integration';
 
   paymentForm: FormGroup;
+
 
   constructor(
     private snackbar: MatSnackBar,
@@ -27,9 +31,47 @@ export class AppComponent implements OnInit {
     })
   }
 
+  openRazorpayBox() {
+    let options = {
+      "key": "rzp_test_ci1GxgtQgJTbfh",
+      "amount": "2000", // 2000 paise = INR 20
+      "name": "Merchant Name",
+      "description": "Purchase Description",
+      "image": "https://angular.io/assets/images/support/angular-404.svg",
+      "handler": function (response) {
+        alert(response.razorpay_payment_id);
+      },
+      "prefill": {
+        "name": "Gaurav Kumar",
+        "email": "test@test.com"
+      },
+      "notes": {
+        "address": "Hello World"
+      },
+      "theme": {
+        "color": "#F37254"
+      },
+      "order_id": ''
+    };
+    var rzp = new Razorpay(options);
+    rzp.open();
+  }
+
+
   getDeductableAmount() {
     console.log('Value', this.paymentForm.value)
-    this.openModal()
+    // this.openModal()
+
+    this.http.post('/api/generateId', this.paymentForm.value).subscribe(
+      (res: any) => {
+        if (res.success) {
+          console.log(res)
+        } else {
+          console.log(res.msg)
+        }
+      }, () => {
+        console.log('Something went wrong.')
+      })
   }
 
   openModal() {
@@ -43,6 +85,11 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.createForm()
+
+    get('https://checkout.razorpay.com/v1/checkout.js', () => {
+      console.log('script loaded')
+    });
+
   }
 
 }
